f603c25825564adb5774c15a2bb88e8e5c0758ab
mtoma <mtoma@DESKTOP-P9KVDUO>
2025-03-17 01:37:59 -0600

    download working

---
+++ PersonalCloud/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.downloadFile = (url) => {
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = ''; // Let browser use the filename from URL
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+};
